087eb1ce52507afa0dfeb19a27152c588c3df7db
Farne23 <michele.farneti23@gmail.com>
2021-02-15 00:19:42 +0100

    Update mieiscripit.js

--- FarnetiMichele4H_Jquery/js/mieiscripit.js
+++ FarnetiMichele4H_Jquery/js/mieiscripit.js
@@ -5,6 +5,8 @@ $(function () {
     var NomiInseriti =[];
     var CognomiInseriti =[];
     var conteggio=0;
+    var nomcog 
+    var dacancellare
 
     $("#btninvio").on("click", function() {
 
@@ -34,12 +36,46 @@ $(function () {
 
     });
 
-    $("#btnconferma").on("click", function() { 
+    $("#btnconferma").on("click", function(){
 
-        $("#elenconominativi").append("<p>"+ nome + " " + cognome+"</p>");
+        $("#listanominativi").append("<li id=listanomi class='row'><div class='col col-md-10'> " + nome + " " + cognome + "</div ><div class='bottonedelete col col-md-2'><img src='images/delete.png' id='btndelete'></div></li>"); 
         NomiInseriti[conteggio]=nome;
         CognomiInseriti[conteggio]=cognome;
         conteggio++;
 
+        $("#listanominativi").ready(function(){
+            
+            $(".bottonedelete").on("click", function(){
+
+
+                dacancellare =  $(this).closest("li"); 
+                nomcog = this.closest("li").innerText;
+
+                $("#ModalConfermaDel").modal();                        
+                    
+            });
+            
+        });
+
+
+
+        //alert(NomiInseriti);
+        
     });
+
+    $("#btnDelConfermata").on("click", function() {
+
+        $(dacancellare).remove();
+
+        for(var j=0; j<conteggio; j++)
+        {
+            if(nomcog==NomiInseriti[j]+" "+CognomiInseriti[j])
+            {
+                NomiInseriti[j]="//";
+                CognomiInseriti[j]="//";
+                break;
+            }
+        } 
+    });
+    
 });
